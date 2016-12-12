2659d926f25879db037cb7a290604619fb103cf4
Cagribeyg <cagribeyg@gmail.com>
2016-12-12 13:12:28 +0200

    Update hash.c

--- hash.c
+++ hash.c
@@ -1,6 +1,6 @@
 /*
 Copyright (C) 2016  Serhat Çetinkaya serhatcetinkayaa@gmail.com
-		    Çağrıbey Güvendik mguvendik@gmail.com
+		    Çağrıbey Güvendik cagribeyg@gmail.com
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
